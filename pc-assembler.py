595063e6864a06d33b1994bc4b1832406966fb21
Hussien <hussienmahmuod@gmail.com>
2021-07-20 02:40:33 +0200

    ability to add parts properly

--- pc-assembler.py
+++ pc-assembler.py
@@ -5,6 +5,7 @@ import requests
 def results_from_uptodate(name: str) -> list:
     found_products = []
     content = requests.get(f'https://uptodate.store/?category=0&s={name}&post_type=products').text
+
     soup = BeautifulSoup(content, 'lxml')
     products = soup.find_all('div', {'class': "products"})
     for product in products:
@@ -12,7 +13,7 @@ def results_from_uptodate(name: str) -> list:
         website_name = website_name_content.text
         link = website_name_content.find('a')['href']
         price = product.find('div', {'class': "product-price"}).text
-        if product.find('div', {'class': "tag out-stock"}) != None:
+        if product.find('div', {'class': "tag out-stock"}) is not None:
             availability = "Out of Stock"
         else:
             availability = "In Stock"
@@ -25,16 +26,11 @@ def results_from_maximumhardware(name: str) -> list:
     found_products = []
     content = requests.get(
         f'https://maximumhardware.store/index.php?category_id=0&search={name}&submit_search=&route=product%2Fsearch',
-        headers={'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36'}
+        headers={
+            'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Mobile Safari/537.36'}
     )
-    # print(content)
-    # print(content.headers)
-    # print(content.text)
-    # with open("page.html", 'w') as html_file:
-    #     print(str(content.text), file=html_file)
 
     soup = BeautifulSoup(content.text, 'lxml')
-    print(soup)
     products = soup.find_all('div', {'class': "product-layout"})
     for product in products:
         website_name_content = product.find('h4')
@@ -50,7 +46,6 @@ def results_from_maximumhardware(name: str) -> list:
         found_products.append((website_name, price, availability, link))
 
     return found_products
-    # return [('i3 10100', '1900', 'out of stock', "www.somewebsite2.com")]
 
 
 def search_for(name: str) -> list:
@@ -73,17 +68,45 @@ if __name__ == '__main__':
     # 4- exporting to CSV file
 
     # (1) adding parts to the list
-    part = input("Enter the product name you want to search for: ")
+    searching = True
+    while searching:
+        part = input("Enter the product name you want to search for: ")
+        results = search_for(part)
+        if len(results) == 0:
+            print("nothing was found!")
 
-    results = search_for(part)
+        # showing the options
+        print("choose (multiple choices are seperated by a comma ','):")
+        for index, result in enumerate(results):
+            print(f'({index + 1}) {result}')
+        print('\n(0) for none (and search for something else)')
+        print('(q) to quit choosing')
 
-    print("choose:")
-    for index, result in enumerate(results):
-        print(f'({index + 1}) {result}')
-    print('(0) for none')
-    choice = int(input('>'))
-    # todo type check
-    if choice != 0:
-        pc_parts.append(results[choice - 1])
+        have_chosen = False
+        while not have_chosen:
+
+            choice = input('>')
+            print()
+
+            if choice == 'q' or choice == 'Q':
+                searching = False
+                break
+            elif choice == '0':
+                break
+            else:
+                numbers = choice.split(',')
+                for number in numbers:
+                    if number.isnumeric():
+                        if int(number) < len(results) + 1:
+                            pc_parts.append(results[int(number) - 1])
+                            print("part added to the list")
+                            have_chosen = True
+                            searching = False
+                        else:
+                            print(f"sorry, option {number} is not available")
+                            print("try again from previous list!")
+                    else:
+                        print("you typed something wrong")
+                        print("try again from previous list!")
+
     print(pc_parts)
-

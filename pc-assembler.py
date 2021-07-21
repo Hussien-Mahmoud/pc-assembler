8a09cafe6adab15c9c395a277190430fde697f7f
Hussien <hussienmahmuod@gmail.com>
2021-07-21 04:40:31 +0200

    added the ability to show the list of parts and modified the adding_parts() function

--- pc-assembler.py
+++ pc-assembler.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def price_cleaner(price: str) -> float:
+    cleaned = ''.join(c for c in price if c.isdigit() or c == '.')
+    return float(cleaned)
+
+
 def results_from_uptodate(name: str) -> list:
     found_products = []
     content = requests.get(f'https://uptodate.store/?category=0&s={name}&post_type=products').text
@@ -12,7 +17,7 @@ def results_from_uptodate(name: str) -> list:
         website_name_content = product.find('h3', {'class': "name"})
         website_name = website_name_content.text
         link = website_name_content.find('a')['href']
-        price = product.find('div', {'class': "product-price"}).text
+        price = price_cleaner(product.find('div', {'class': "product-price"}).text)
         if product.find('div', {'class': "tag out-stock"}) is not None:
             availability = "Out of Stock"
         else:
@@ -36,7 +41,7 @@ def results_from_maximumhardware(name: str) -> list:
         website_name_content = product.find('h4')
         website_name = website_name_content.text.strip()
         link = website_name_content.find('a')['href']
-        price = str(product.find('div', {'class': "price"}).text).strip()
+        price = price_cleaner(str(product.find('div', {'class': "price"}).text).strip())
         if product.find('div', {'class': "label-stock label label-success"}) is not None:
             availability = "Out of Stock"
         else:
@@ -81,24 +86,65 @@ def adding_parts(the_list: list) -> None:
             else:
                 numbers = choice.split(',')
                 for number in numbers:
+                    # check if numeric
                     if number.isnumeric():
+                        # check if option is in the list
                         if int(number) < len(results) + 1:
-                            the_list.append(results[int(number) - 1])
-                            print("part added to the list")
-                            have_chosen = True
-                            searching = False
-                            continue
+                            # check for duplicates
+                            if results[int(number) - 1] not in the_list:
+                                the_list.append(results[int(number) - 1])
+                                print("part added to the list")
+                                have_chosen = True
+                                searching = False
+                                continue
+                            else:
+                                print(f"sorry, choice number {number} already exists")
                         else:
                             print(f"sorry, option {number} is not available")
-                            have_chosen = False
-                            searching = True
-                            break
                     else:
                         print("you typed something wrong")
-                        have_chosen = False
-                        searching = True
-                        break
-                print("\ntry again from previous list!")
+
+                    # this will execute if anything wrong happened
+                    print("\ntry again from previous list!")
+                    have_chosen = False
+                    searching = True
+                    break
+
+
+def show_parts(the_list: list) -> None:
+    len1 = len(the_list[0][0])
+    len2 = len(f'{the_list[0][1]:.2f}')
+    len3 = len(the_list[0][2])
+    len4 = len(the_list[0][3])
+
+    if len(the_list) > 1:
+        index = 0
+        while index < len(the_list) - 1:
+            if len1 < len(the_list[index + 1][0]):
+                len1 = len(the_list[index + 1][0])
+
+            if len2 < len(f'{the_list[index + 1][1]:.2f}'):
+                len2 = len(f'{the_list[index + 1][1]:.2f}')
+
+            if len3 < len(the_list[index + 1][2]):
+                len3 = len(the_list[index + 1][2])
+
+            if len4 < len(the_list[index + 1][3]):
+                len4 = len(the_list[index + 1][3])
+
+            index += 1
+
+    for name, price, availability, link in the_list:
+        print(name + ' ' * (len1 - len(name)), sep='', end=' | ')
+        print(f'{price:.2f}' + ' ' * (len2 - len(f'{price:.2f}')), sep='', end=' | ')
+        print(availability + ' ' * (len3 - len(availability)), sep='', end=' | ')
+        print(link + ' ' * (len4 - len(link)), sep='', end=' | ')
+        print()
+        # print(f'{name:<len1}, {price:<len2}, {availability:<len3}, {link:<len4}')
+
+
+def removing_parts():
+    pass
 
 
 part_info = ('name', 'price', 'availability', 'link')
@@ -115,7 +161,7 @@ if __name__ == '__main__':
         if len(pc_parts) == 0:
             adding_parts(pc_parts)
         else:
-            print("choose what you want to do!\n")
+            print("\n****choose what you want to do!****\n")
             print("(1) adding parts to the list")
             print("(2) removing parts from the list")
             print("(3) show all chosen products")
@@ -123,11 +169,11 @@ if __name__ == '__main__':
             print("(q) to exit the program")
             option = input('>')
             if option == '1':
-                adding_parts()
+                adding_parts(pc_parts)
             elif option == '2':
-                pass
+                removing_parts()
             elif option == '3':
-                pass
+                show_parts(pc_parts)
             elif option == '4':
                 pass
             elif option == 'q' or option == 'Q':
